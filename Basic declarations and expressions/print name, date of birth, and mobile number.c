9e744d65e43a859453edf6a9f2f4ff2d86cd0307
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-07-10 07:56:53 +0530

    Create print name, date of birth, and mobile number.c

---
+++ Basic declarations and expressions/print name, date of birth, and mobile number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    char n[50]="Alexandra Abramov";
+    char bd[50]="July 14, 1975";
+    char mob[15]="99-9999999999";
+
+    printf("Name  : %s\n",n);
+
+
+    printf("DOB   : %s\n",bd);
+
+
+    printf("Mobile: %s",mob);
+
+}
