30065e899d0f8b67130f46100f069bfa2ae34057
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-07-10 08:00:32 +0530

    Create print a block F using the hash (#), where the F has a height of six characters and width of five and four characters. And also print a very large 'C'.c

---
+++ Basic declarations and expressions/print a block F using the hash (#), where the F has a height of six characters and width of five and four characters. And also print a very large 'C'.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main()
+{
+
+
+   printf("#######\n");
+
+   printf("#\n");
+
+   printf("#\n");
+
+   printf("#####\n");
+
+   printf("#\n");
+
+   printf("#\n");
+
+   printf("#");
+
+
+
+
+
+    printf("   ######\n");
+    printf(" ##       ##\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf(" ##       ##\n");
+    printf("   ######");
+
+
+}
