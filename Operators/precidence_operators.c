9f117c010931ca141606cbf52f7231185ea70fb9
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-25 13:02:35 +0530

    Create precidence_operators.c

---
+++ Operators/precidence_operators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+    int x=10;
+    int y=2;
+    int z=5;
+    int a=7;
+    int b=0;
+    int c=13;
+    int d=20;
+
+    printf("\nAnswer P:%d\n\n",y*z+x-y);
+    printf("\nAnswer Q:%d\n\n",x+y*(z-y));
+    printf("\nAnswer R:%d\n\n",a&&b*c<=x);
+    printf("\nAnswer S:%d\n\n",d*y-x-(z||b));
+
+
+
+}
