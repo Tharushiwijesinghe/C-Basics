4b1c250992dc6b9417f428f6618acf510a29ce10
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-25 13:01:44 +0530

    Create logical_operators.c

---
+++ Operators/logical_operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+ printf("%d\n",0&&0);
+ printf("%d\n",24&&0);
+ printf("%d\n\n",24&&24);
+ printf("%d\n",0||0);
+ printf("%d\n",25||0);
+ printf("%d\n\n",25||25);
+ printf("%d\n",!25);
+ printf("%d\n",!0);
+
+
+
+}
