b04148bfc968fb1fbcf4db471368720e9d8ac6bd
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-25 12:50:05 +0530

    Create do_while_nested_loop.c

---
+++ loops/do_while_nested_loop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=1,j=3;
+
+    do{
+
+
+        do{
+        printf("%d",j);
+        j--;
+
+        }while(j>0);
+
+        i++;
+        printf("%d",i);
+
+    }while(i<4);
+}
