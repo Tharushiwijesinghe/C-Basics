2df743a6baa3f4905f36e8135edc5a3da3a40b6c
Tharushiwijesinghe <134124219+Tharushiwijesinghe@users.noreply.github.com>
2023-10-26 22:37:06 +0530

    Create if_else.c

---
+++ Condition/if_else.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    int i=8;
+    int j=5;
+    float x=0.005;
+    char d='d';
+    char c='c';
+    int a=2;
+    int b=3;
+    int h=4;
+
+    printf("\n%d %d %d %d %d",a*((i/j)+(h*(j-b))%(i+j-b)));
+
+
+}
